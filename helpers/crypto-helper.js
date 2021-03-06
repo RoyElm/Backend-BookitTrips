1f845aeff7a147d823e95798023fc6ef3389b714
RoyElm <roye456@gmail.com>
2021-03-06 18:56:35 +0200

    UI Changed

--- helpers/crypto-helper.js
+++ helpers/crypto-helper.js
@@ -1,6 +1,6 @@
 const crypto = require("crypto");
 
-const salt = "*";
+const salt = "BookThingsAtSexyWay";
 
 function hash(plainText) {
 
--- helpers/jwt-helper.js
+++ helpers/jwt-helper.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const key = "*";
+const key = "AmazingLocations";
 
 function getNewToken(payload) {
     return jwt.sign(payload, key, { expiresIn: "30m" });
